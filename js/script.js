71d485f05b376e8ed567291b888a499040e8cd6d
Sung <sung272012@hotmail.com>
2024-07-12 23:00:43 -0400

    Created 16x16 grid using flexbox

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 const container = document.querySelector("#container");
 
 for (let row = 1; row < 17; row++) {
-    for (let col = 1; col < 17; col++) {
-        const div = document.createElement("div");
-        div.setAttribute("id", `row${row}col${col}`);
-        container.appendChild(div);
-    }
+    const div = document.createElement("div");
+    div.setAttribute("id", `row${row}`);
+    container.appendChild(div);
 }
+
+let row = 1;
+while (row < 17) {
+    for (let col = 1; col < 17; col++) {
+        const parent = document.querySelector(`#row${row}`);
+        const column = document.createElement("div");
+        column.setAttribute("id", `col`);
+        parent.appendChild(column);
+    }
+    row++
+}
+
